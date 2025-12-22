17e975c2291e3b7e77eee1f1b53665dc7e8a46a7
AyushRaj <ayushraj620621@gmail.com>
2025-12-22 11:31:00 +0530

    Fixed loops folder structure (removed wrong folders, added correct ones)

--- create_projects.bat
+++
@@ -1,7 +0,0 @@
-cd Loops
-for %%f in (17_FibonacciSeries 18_PrimeNumber 19_ArmstrongNumber 20_ReverseAndPalindrome 21_GCD_LCM 22_PascalsTriangle 23_BinaryToDecimal 24_DiamondPattern 25_FactorialLarge 26_GuessingGame 27_DigitalRoot 28_ContinueUsage 29_MenuSystem 30_StrongNumber 31_SearchWithGoto) do (
-  mkdir %%f
-  cd %%f
-  dotnet new console
-  cd ..
-)
